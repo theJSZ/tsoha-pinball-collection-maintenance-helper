d8dedd17b827a384c4835f01d3548c58a74aec56
Jussi Rantala <jussi.v.rantala@helsinki.fi>
2023-02-19 17:45:59 +0200

    update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,6 +1,14 @@
+DROP TABLE IF EXISTS collections CASCADE;
+DROP TABLE IF EXISTS machines CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS issues CASCADE;
+DROP TABLE IF EXISTS comments CASCADE;
+DROP TABLE IF EXISTS rights CASCADE;
+
+
 CREATE TABLE collections (id SERIAL PRIMARY KEY, name TEXT);
 CREATE TABLE machines (id SERIAL PRIMARY KEY, name TEXT, collection_id INTEGER REFERENCES collections ON DELETE CASCADE);
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, email TEXT, password TEXT);
-CREATE TABLE issues (id SERIAL PRIMARY KEY, machine_id REFERENCES machines, user_id REFERENCES users, created_at TIMESTAMP, content TEXT, closed_by INTEGER REFERENCES users, severity INTEGER);
+CREATE TABLE issues (id SERIAL PRIMARY KEY, machine_id INTEGER REFERENCES machines, user_id INTEGER REFERENCES users, created_at TIMESTAMP, content TEXT, closed_by INTEGER REFERENCES users, severity INTEGER);
 CREATE TABLE comments (id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users, issue_id INTEGER REFERENCES issues, created_at TIMESTAMP, content TEXT);
 CREATE TABLE rights (user_id INTEGER REFERENCES users, collection_id INTEGER REFERENCES collections, is_admin BOOLEAN);
