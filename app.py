a66be684ffef8c0c718b8153871b05055d71082f
Jussi Rantala <jussi.v.rantala@helsinki.fi>
2023-02-21 22:17:57 +0200

    Add Fly.io config

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from flask import Flask
 from os import getenv
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-
+# app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1)
 app.secret_key = getenv("SECRET_KEY")
 
 import routes
